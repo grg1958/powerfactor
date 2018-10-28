dc5d1ea5b0cda1bf71dbc81a560f03732ab06c39
grg <grg1958@gmail.com>
2018-10-28 10:37:33 -0500

    Initial Commit

---
+++ Powerfactor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Powerfactor
+//
+//  Created by Greg Gudenburr on 10/28/18.
+//  Copyright © 2018 Greg Gudenburr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
